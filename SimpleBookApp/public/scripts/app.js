78761b676788bc15a558df570c434905af5ed3f4
Rahul Ekbote <rahulek@gmail.com>
2022-10-16 11:21:38 +0530

    Simple Book app - basic JSX and Event handler

--- SimpleBookApp/public/scripts/app.js
+++ SimpleBookApp/public/scripts/app.js
@@ -1,3 +1,60 @@
 "use strict";
 
-console.log("Simple Book app: script loaded.");
+//application specific information
+//Store - title, subtitle and empty list of books.
+var app = {
+  title: "Simple Book Listing Application",
+  subtitle: "Books are your teachers.",
+  books: []
+};
+
+//Function to handle the form submission events
+var onFormSubmit = function onFormSubmit(e) {
+  //1. Every time the form is submitted, browser refreshes the page
+  //   Click on the button to see the page link change
+  //   and the page refreshed.
+  //
+  //2. This is the default browser behavaior which can be prevented
+  //
+  //3. How and where to get the input value entered?
+  //   Check the console.log
+  //   e.target.elements.input.value
+
+  e.preventDefault(); //don't let the browser refresh the page
+  console.log(e); //log the event
+};
+
+var jsx = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "h2",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    "There are ",
+    app.books.length,
+    " books in the list so far."
+  ),
+  React.createElement(
+    "form",
+    { onSubmit: onFormSubmit },
+    React.createElement("input", { type: "text", name: "booktitle", placeholder: "Book title" }),
+    React.createElement(
+      "button",
+      { type: "submit" },
+      "Enter it!"
+    )
+  )
+);
+
+var appRoot = ReactDOM.createRoot(document.querySelector("#app"));
+appRoot.render(jsx);
--- SimpleBookApp/src/app.js
+++ SimpleBookApp/src/app.js
@@ -1,1 +1,40 @@
-console.log("Simple Book app: script loaded.");
+//application specific information
+//Store - title, subtitle and empty list of books.
+let app = {
+  title: "Simple Book Listing Application",
+  subtitle: "Books are your teachers.",
+  books: [],
+};
+
+//Function to handle the form submission events
+const onFormSubmit = (e) => {
+  //1. Every time the form is submitted, browser refreshes the page
+  //   Click on the button to see the page link change
+  //   and the page refreshed.
+  //
+  //2. This is the default browser behavaior which can be prevented
+  //
+  //3. How and where to get the input value entered?
+  //   Check the console.log
+  //   e.target.elements.input.value
+
+  e.preventDefault(); //don't let the browser refresh the page
+  console.log(e); //log the event
+};
+
+const jsx = (
+  <div>
+    <h1>{app.title}</h1>
+    <h2>{app.subtitle}</h2>
+    <p>There are {app.books.length} books in the list so far.</p>
+
+    {/* form to allow new book title entry */}
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="booktitle" placeholder="Book title" />
+      <button type="submit">Enter it!</button>
+    </form>
+  </div>
+);
+
+const appRoot = ReactDOM.createRoot(document.querySelector("#app"));
+appRoot.render(jsx);
